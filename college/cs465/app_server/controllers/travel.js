1db3128eb7151d49b97679374612a48ed2b22274
Austin <38254442+cs-dad@users.noreply.github.com>
2025-07-27 17:39:21 -0400

    Push the required code changes to complete Module 3-1

--- college/cs465/app_server/controllers/travel.js
+++ college/cs465/app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./college/cs465/app_server/data/trips.json', 'utf8'));
+
+
 /* GET travel page for CS465 */
 const travel = (req, res) => {
-  res.render('travel', { title: "Travlr Gateways" });
+  res.render('travel', { title: "Travlr Gateways", trips });
 }
 
 module.exports = {
